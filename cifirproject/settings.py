a99e93000eca7b475509e5a37f3d0f7ae9a78b11
leam123 <garcia.leamor1@gmail.com>
2022-02-19 01:29:19 +0800

    Update settings.py

--- cifirproject/settings.py
+++ cifirproject/settings.py
@@ -115,9 +115,9 @@ DATABASES = {
 
 # keep this
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
 
